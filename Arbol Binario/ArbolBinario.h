dd63881848a115155186e9631e285b49394b28aa
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-31 00:59:00 -0500

    Update ArbolBinario.h

--- Arbol Binario/ArbolBinario.h
+++ Arbol Binario/ArbolBinario.h
@@ -1,3 +1,9 @@
+/*******************************************
+Fecha: 26 marzo 2025
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
 #ifndef __ARBOLBINARIO__H__
 #define __ARBOLBINARIO__H__
 

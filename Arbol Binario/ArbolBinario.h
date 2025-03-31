ea49f2c2d10063788a0a51e270e4b340f59e020a
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-31 00:51:25 -0500

    Update ArbolBinario.h

--- Arbol Binario/ArbolBinario.h
+++ Arbol Binario/ArbolBinario.h
@@ -1,46 +1,92 @@
-#ifndef __ARBOLBINARIO__H__
-#define __ARBOLBINARIO__H__
-
+// ArbolBinario.hxx
+#include "ArbolBinario.h"
 #include "NodoBinario.h"
 
 using namespace std;
 
+// Constructor de ArbolBinario
+// Inicializa el árbol con una raíz nula
 template<class T>
-class ArbolBinario {
-protected:
-    NodoBinario<T> *raiz;
-public:
-    // Constructor por defecto
-    ArbolBinario();
-    
-    // Verifica si el árbol está vacío
-    bool esVacio();
-    
-    // Devuelve el dato almacenado en la raíz
-    T& datoRaiz();
-    
-    // Calcula la altura del árbol
-    int altura();
-    
-    // Calcula el tamaño del árbol
-    int tamano();
-    
-    // Inserta un nuevo elemento en el árbol
-    void insertar(T& val);
-    
-    // Elimina un elemento del árbol
-    bool eliminar(T& val);
-    
-    // Busca un elemento en el árbol
-    NodoBinario<T>* buscar(T& val);
-    
-    // Recorridos del árbol
-    void preOrden();
-    void inOrden();
-    void posOrden();
-    void nivelOrden();
-};
+ArbolBinario<T>::ArbolBinario() {
+    this->raiz = NULL;
+}
 
-#include "ArbolBinario.hxx"
-#endif
+// Verifica si el árbol está vacío
+template<class T>
+bool ArbolBinario<T>::esVacio() {
+    return this->raiz == NULL;
+}
 
+// Devuelve el dato de la raíz
+template<class T>
+T& ArbolBinario<T>::datoRaiz() {
+    return this->raiz->obtenerDato();
+}
+
+// Calcula la altura del árbol
+template<class T>
+int ArbolBinario<T>::altura() {
+    return this->raiz ? this->raiz->altura() : 0;
+}
+
+// Calcula el tamaño del árbol
+template<class T>
+int ArbolBinario<T>::tamano() {
+    return this->raiz ? this->raiz->tamano() : 0;
+}
+
+// Inserta un nuevo valor en el árbol
+template<class T>
+void ArbolBinario<T>::insertar(T& val) {
+    if (this->raiz) {
+        this->raiz->insertar(val);
+    } else {
+        this->raiz = new NodoBinario<T>();
+        this->raiz->fijarDato(val);
+    }
+}
+
+// Elimina un nodo con el valor dado
+template<class T>
+bool ArbolBinario<T>::eliminar(T& val) {
+    NodoBinario<T>* borrar = this->buscar(val);
+    if (!borrar) return false;
+    
+    if (borrar->obtenerHijoIzq()) {
+        NodoBinario<T>* nuevo = borrar->obtenerHijoIzq()->extremo_der();
+        borrar->fijarDato(nuevo->obtenerDato());
+        delete nuevo;
+    } else if (borrar->obtenerHijoDer()) {
+        NodoBinario<T>* nuevo = borrar->obtenerHijoDer()->extremo_izq();
+        borrar->fijarDato(nuevo->obtenerDato());
+        delete nuevo;
+    }
+    return true;
+}
+
+// Busca un nodo con el valor dado
+template<class T>
+NodoBinario<T>* ArbolBinario<T>::buscar(T& val) {
+    return this->raiz ? this->raiz->buscar(val) : NULL;
+}
+
+// Recorridos del árbol
+template<class T>
+void ArbolBinario<T>::preOrden() {
+    if (this->raiz) this->raiz->preOrden();
+}
+
+template<class T>
+void ArbolBinario<T>::inOrden() {
+    if (this->raiz) this->raiz->inOrden();
+}
+
+template<class T>
+void ArbolBinario<T>::posOrden() {
+    if (this->raiz) this->raiz->posOrden();
+}
+
+template<class T>
+void ArbolBinario<T>::nivelOrden() {
+    if (this->raiz) this->raiz->nivelOrden();
+}

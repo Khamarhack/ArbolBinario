a204b93c616eb71abb3b1c59973a2ef2a92729fe
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-31 01:00:18 -0500

    Update PruebaArbolBinario.cpp

--- Arbol Binario/PruebaArbolBinario.cpp
+++ Arbol Binario/PruebaArbolBinario.cpp
@@ -1,3 +1,9 @@
+/*******************************************
+Fecha: 26 marzo 2025
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
 #include<iostream>
 #include "ArbolBinario.h"
 #include "NodoBinario.h"

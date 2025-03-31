f388c80d7d6d3c1a77606e168226fb791dc4af84
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-31 00:38:48 -0500

    Update ArbolBinario.h

--- Arbol Binario/ArbolBinario.h
+++ Arbol Binario/ArbolBinario.h
@@ -8,21 +8,22 @@ using namespace std;
 template<class T>
 class ArbolBinario {
   protected:
-    NodoBinario<T> *raiz;
+    NodoBinario<T> *raiz; // Raíz del árbol
 public:
-	ArbolBinario();
-    bool esVacio();
-    T& datoRaiz();
-    int altura();
-    int tamano();
-    void insertar(T& val);
-    bool eliminar(T& val);
-    NodoBinario<T>*  buscar(T& val);
-    void preOrden();
-    void inOrden();
-    void posOrden();
-    void nivelOrden();
+    ArbolBinario(); // Constructor
+    bool esVacio(); // Verifica si el árbol está vacío
+    T& datoRaiz(); // Devuelve el dato de la raíz
+    int altura(); // Calcula la altura del árbol
+    int tamano(); // Calcula el número de nodos en el árbol
+    void insertar(T& val); // Inserta un valor en el árbol
+    bool eliminar(T& val); // Elimina un valor del árbol
+    NodoBinario<T>* buscar(T& val); // Busca un nodo con el valor dado
+    void preOrden(); // Recorre el árbol en preorden
+    void inOrden(); // Recorre el árbol en inorden
+    void posOrden(); // Recorre el árbol en posorden
+    void nivelOrden(); // Recorre el árbol por niveles
 };
 
 #include "ArbolBinario.hxx"
 #endif
+
